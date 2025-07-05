f00452e06598893672a6b25221a6196438dfc6ee
v1tyka <racinvictor@gmail.com>
2025-07-05 21:02:06 +0200

    update

---
+++ src/task-1.ts
@@ -0,0 +1,11 @@
+type Name = string;
+type Age = number;
+type IsOnline = boolean;
+
+const name: Name = "Alice";
+const age: Age = 30;
+const isOnline: IsOnline = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
